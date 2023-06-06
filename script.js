ee9a08d1d99be1ce1d5e47363111dab1d9888568
SeddiMK <web.egorovm@gmail.com>
2023-06-06 15:36:11 +0700

    bankCard-Js

--- script.js
+++ script.js
@@ -2,10 +2,19 @@ document.querySelector('#card-number').oninput = function () {
   let cardNum = this.value;
   if (cardNum.trim().length > 5) {
     let cardInfo = new CardInfo(cardNum.trim(), {
-      bankLogosPath: './node_modules/card-info/dist/banks-logos',
-      brandsLogosPath: './node_modules/card-info/dist/brands-logos',
+      banksLogosPath: './node_modules/card-info/dist/banks-logos/',
+      brandsLogosPath: './node_modules/card-info/dist/brands-logos/',
     });
     //bank name
     console.log(cardInfo.bankName);
+    console.log(cardInfo.bankLogo);
+    console.log(cardInfo.brandLogo);
+    console.log(cardInfo.backgroundColor);
+    if (cardInfo.bankName !== null) {
+      document.querySelector('.front-card__img').src = cardInfo.bankLogo;
+      document.querySelector('.front-card__logo').src = cardInfo.brandLogo;
+      document.querySelector('.front-card').style.backgroundColor =
+        cardInfo.backgroundColor;
+    }
   }
 };
